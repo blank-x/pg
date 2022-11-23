85fd4942adbced486a565c1544b73098cb1b9d44
zhaoshaoyong1970 <zhaoshaoyong1970@foxmail.com>
2022-11-23 10:27:04 +0800

    add

---
+++ ts-pg/module.ts
@@ -0,0 +1,7 @@
+import xx from './a.png';
+
+console.log(xx)
+
+type a = [string, ...boolean[], null];
+
+const data:a = ['1', false, null];
--- ts/module.ts
+++
@@ -1,3 +0,0 @@
-import xx from './a.png';
-
-console.log(xx)
